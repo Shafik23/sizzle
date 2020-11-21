b2223012750c73155b0992f24879d7ec30bc5b0c
Shafik Amin <shafik23@gmail.com>
2020-11-21 05:01:45 -0700

    Added main example

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,12 @@
 module Main where
 
+import File
 import Lib
 
 main :: IO ()
-main = helloSizzle
+main = do
+  writeLines "foo.txt" ["a", "b", "c"]
+  result <- readLines "foo.txt"
+  let stuff = either id unlines result
+  print stuff
+  return ()
