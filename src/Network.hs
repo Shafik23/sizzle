cad9bfb62221154f666495cf237c54db372f707f
Shafik Amin <shafik23@gmail.com>
2021-01-10 02:43:34 -0700

    Added working Network module!

--- src/Network.hs
+++ src/Network.hs
@@ -1,6 +1,32 @@
 module Network
-  (
+  ( httpGet,
   )
 where
 
 import Control.Exception
+import qualified Data.ByteString.Char8 as B8
+import Data.Text
+import Data.Text.Encoding
+import Network.HTTP.Simple
+
+type Url = String
+
+data Method = GET | POST | PATCH | DEL deriving (Show)
+
+httpHelper :: Method -> Url -> IO Text
+httpHelper method url = do
+  request <- parseRequest $ show method ++ " " ++ url
+  response <- httpBS request
+  return $ (decodeUtf8 . getResponseBody) response
+
+httpGet :: Url -> IO Text
+httpGet = httpHelper GET
+
+httpPost :: Url -> IO Text
+httpPost = httpHelper POST
+
+httpPatch :: Url -> IO Text
+httpPatch = httpHelper PATCH
+
+httpDelete :: Url -> IO Text
+httpDelete = httpHelper DEL
