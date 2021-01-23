65dca03bc42fed4ce6b9d0288a0a1e5f5638373e
Shafik Amin <shafik23@gmail.com>
2021-01-23 04:58:54 -0700

    Added a whole bunch of dependencies, made main a Scotty app!

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,29 @@
 module Main where
 
+import Control.Monad
+import Control.Monad.Trans
+import Network.Wai.Middleware.RequestLogger
 import Sizzle
+import System.Random (newStdGen, randomRs)
+import Web.Scotty
 
 main :: IO ()
-main = do
-  writeLines "foo.txt" ["a", "b", "c"]
-  result <- readLines "foo.txt"
-  let stuff = either id unlines result
-  print stuff
-  return ()
+main = scotty 3000 $ do
+  middleware logStdoutDev
+
+  get "/" $ text "Welcome to a Sizzle, an amalgamation library to help web development"
+
+  get "/random" $ do
+    g <- liftIO newStdGen
+    json $ take 20 $ randomRs (1 :: Int, 100) g
+
+  get "/jsonExample" $ do
+    json ("Something" :: String)
+
+  get "/header" $ do
+    agent <- header "User-Agent"
+    maybe (raise "User-Agent header not found!") text agent
+
+  get "/static/:file" $ do
+    f <- param "file"
+    file $ "static/" ++ f
